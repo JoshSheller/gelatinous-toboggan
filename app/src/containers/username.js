53ec2bf25f5459c7faf1e960a18249d1bcd942ae
joshsheller <joshua.a.sheller@gmail.com>
2016-03-28 16:37:30 -0700

    Convert username and email to lowercase before user creation or sign in requests

--- app/src/containers/username.js
+++ app/src/containers/username.js
@@ -31,7 +31,8 @@ class Username extends Component {
   }
 
   onEnter() {
-    this.props.updateUser(this.props.userId, { username: this.state.username, token: this.props.token });
+    const usernameToLowercase = this.state.username.toLowerCase();
+    this.props.updateUser(this.props.userId, { username: usernameToLowercase, token: this.props.token });
     this.props.navigator.push({ name: 'phone' })
   }
 
